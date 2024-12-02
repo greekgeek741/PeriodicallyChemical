8a91322bcd7edce785481d192a6b927ce6998e97
C4Fey <greekgeek741@icloud.com>
2024-12-01 19:43:03 -0600

    Initial Commit

---
+++ src/main/java/net/c4fey/perichem/PeriodicallyChemicalDataGenerator.java
@@ -0,0 +1,11 @@
+package net.c4fey.perichem;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class PeriodicallyChemicalDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
